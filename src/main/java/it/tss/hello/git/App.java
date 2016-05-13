37c5a13d36e5f66fa730a3bb09f9ded3d7b87235
tss <tss@debian-tss24>
2016-05-13 11:57:48 +0200

    Prima modifica

--- src/main/java/it/tss/hello/git/App.java
+++ src/main/java/it/tss/hello/git/App.java
@@ -12,5 +12,6 @@ package it.tss.hello.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git.");
+        System.out.println("HPrima modifica.");
     }
 }
